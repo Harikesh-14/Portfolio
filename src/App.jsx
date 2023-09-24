c6c2d923c70ac0256879e64b9db9a9247beb1d0d
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2023-09-24 15:28:54 +0530

    Basic structure of the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Navbar from './components/Navbar/Navbar'
+
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+import { Link } from 'react-scroll'
+
+export default function Navbar() {
+  return (
+    
+    <nav className="navbar">
+        {/* logo of the website */}
+        <img src={logo} alt="Logo" className='logo'/>
+
+        {/* Sections of the website */}
+        <div className="desktopMenu">
+            <Link className="desktopMenuListItems">Home</Link>
+            <Link className="desktopMenuListItems">About</Link>
+            <Link className="desktopMenuListItems">Portfolio</Link>
+            <Link className="desktopMenuListItems">Clients</Link>
+        </div>
+
+        {/* Contact Me Button */}
+        <button className="desktopMenuButton">
+            <img src="" alt="" className='desktopMenuImg' /> Contact Me
+        </button>
+    </nav>
+  )
+}
