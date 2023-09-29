34115a57ad3563235b34e6c9a096b25ec5e29091
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2023-09-29 17:45:31 +0530

    Updated the structure of Skills section

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -2,7 +2,38 @@ import React from 'react'
 import './Skills.css'
 
 export default function Skills() {
-  return (
-    <div>Skills</div>
-  )
+    return (
+        <section id="skills">
+            <span className="skillTitle">What do I do</span>
+            <span className="skillDesc">I'm a passionate full-stack web developer with a creative mindset
+                and a penchant for turning ideas into reality. With a strong foundation
+                in both front-end and back-end technologies, I bring websites and
+                web applications to life, crafting seamless user experiences and robust
+                functionality.</span>
+
+            <div className="skillBars">
+                <div className="skillBar">
+                    <img src="" alt="" className="skillBarImg" />
+                    <div className="skillBarText">
+                        <h2></h2>
+                        <p></p>
+                    </div>
+                </div>
+                <div className="skillBar">
+                    <img src="" alt="" className="skillBarImg" />
+                    <div className="skillBarText">
+                        <h2></h2>
+                        <p></p>
+                    </div>
+                </div>
+                <div className="skillBar">
+                    <img src="" alt="" className="skillBarImg" />
+                    <div className="skillBarText">
+                        <h2></h2>
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
