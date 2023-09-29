ad3f004c8e74f3f3b0f47e95db5017c428cdc1d3
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2023-09-29 12:42:56 +0530

    Updated the css of Intro

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -14,7 +14,7 @@ export default function Intro() {
             <p className="introPara">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae sequi repellendus deleniti eligendi quae! Numquam ipsum fugiat aspernatur assumenda, soluta excepturi nemo? A, iusto laborum? Deserunt fugit ut a quibusdam?
             </p>
-            <Link><button className='btn'><img src={hireMeLogo} alt="Hire Me" /> Hire Me</button></Link>
+            <Link><button className="btn"><img src={hireMeLogo} alt="Hire Me" /> Hire Me</button></Link>
         </div>
 
         <img src={myPhoto} alt="Me" className='myPhoto' />
