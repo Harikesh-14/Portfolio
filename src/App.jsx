d3ea9812e801edc6ab29f49f11c66b36bb2ce026
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2023-09-29 17:33:36 +0530

    Created a component from Skills section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar/Navbar'
 import Intro from './components/Intro/Intro';
+import Skills from './components/Skills/Skills';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Intro />
+      <Skills />
     </div>
   );
 }
---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Skills.css'
+
+export default function Skills() {
+  return (
+    <div>Skills</div>
+  )
+}
