a480f300e51df0bb56ff81baf2ec61e360735e7e
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2024-04-07 12:40:28 +0530

    Added framer motion in the project

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,17 @@
+"use client";
+
 import React from 'react'
+import { motion } from 'framer-motion'
 
 function Header() {
   return (
     <header className='z-[999] relative'>
-        <div className='fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full'>
-        </div>
+        <motion.div 
+          className='fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full'
+          initial={{ y: -100, x: '-50%', opacity: 0 }}
+          animate={{ y: 0, x: '-50%', opacity: 1 }}
+        >
+        </motion.div>
     </header>
   )
 }
