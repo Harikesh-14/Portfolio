6e068aa82319c53a001d0b6fc7d41a880214ea81
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2024-04-08 23:00:42 +0530

    Finished skills section:Added advanced animations

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,10 +1,25 @@
 "use client"
 
 import React from 'react'
+import { delay, motion } from 'framer-motion'
 import SectionHeading from './section-heading'
 import { skillsData } from '@/lib/data'
 
 import { useSectionInView } from '@/lib/hook'
+
+const fadeInAnimationVariants = {
+  initial: {
+    opacity: 0,
+    y: 100
+  },
+  animate: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.05 * index,
+    }
+  })
+}
 
 function Skills() {
   const { ref } = useSectionInView('Skills')
@@ -20,12 +35,19 @@ function Skills() {
       <ul className='flex flex-wrap justify-center gap-2 text-gray-800'>
         {
           skillsData.map((skill, index) => (
-            <li
+            <motion.li
               className='bg-white border border-black/[0.1] rounded-xl px-4 py-2'
               key={index}
+              variants={fadeInAnimationVariants}
+              initial='initial'
+              whileInView='animate'
+              viewport={{
+                once: true
+              }}
+              custom={index}
             >
               {skill}
-            </li>
+            </motion.li>
           ))
         }
       </ul>
